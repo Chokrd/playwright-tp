1c11098a504712ce82d2b04cfc1c9aadc95d9de1
chokrd <rkdzalamitte@gmail.com>
2025-11-10 09:49:48 +0100

    Codegen

--- TP1-Introduction/tests/generated.spec.ts
+++ TP1-Introduction/tests/generated.spec.ts
@@ -1,13 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc/#/');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('acheter le jus a joel bms');
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
-  await page.getByRole('checkbox', { name: 'Toggle Todo' }).check();
-  await page.getByRole('link', { name: 'Completed' }).click();
-  await page.getByRole('link', { name: 'Active' }).click();
-  await page.getByRole('link', { name: 'All' }).click();
-  await page.getByRole('checkbox', { name: 'Toggle Todo' }).uncheck();
-  await page.getByRole('checkbox', { name: 'Toggle Todo' }).check();
+test('ajout d’une tâche', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('Acheter le jus à Joël');
+  await page.keyboard.press('Enter');
+  await expect(page.getByText('Acheter le jus à Joël')).toBeVisible();
 });
